9ee9b6a5f007884d8e15fefc32ef78b48757ccf4
Martin Kuban <martin.kuban@physik.hu-berlin.de>
2020-06-08 15:51:34 +0200

    Update setup.py, README.md files

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nomadDOSfingerprints",
+    version="0.1",
+    author="Martin Kuban",
+    author_email="kuban@physik.hu-berlin.de",
+    description="An implementation of DOS fingerprints for the NOMAD Laboratory.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.physik.hu-berlin.de/kuban/nomad-dos-fingerprints",
+    install_requires = ['numpy', 'bitarray'],
+    packages=['nomad_dos_fingerprints']
+)
