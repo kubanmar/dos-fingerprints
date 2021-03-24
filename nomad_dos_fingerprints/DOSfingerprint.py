a652e8aedeaf7df51bae4549803c5d13426e4822
Martin Kuban <kuban@physik.hu-berlin.de>
2021-03-24 19:19:37 +0100

    new options to converte data for fp creation WIP fp generation refactor

--- nomad_dos_fingerprints/DOSfingerprint.py
+++ nomad_dos_fingerprints/DOSfingerprint.py
@@ -1,6 +1,7 @@
 import numpy as np
 from bitarray import bitarray
 from functools import partial
+from collections.abc import Callable
 
 from .grid import Grid
 from .similarity import tanimoto_similarity
@@ -17,10 +18,17 @@ class DOSFingerprint():
         self.grid_id = None
         self.set_similarity_function(similarity_function, **kwargs)
 
-    def calculate(self, dos_energies, dos_values, grid_id = 'dg_cut:56:-2:7:(-10, 5)', unit_cell_volume = 1, n_atoms = 1):
-        energy, dos = self._convert_dos(dos_energies, dos_values, unit_cell_volume = unit_cell_volume, n_atoms = n_atoms)
+    def calculate(self, dos_energies, dos_values, grid_id = 'dg_cut:56:-2:7:(-10, 5)', convert_data = 'Enc', unit_cell_volume = 1, n_atoms = 1):
+        if convert_data == 'Enc':
+            energy, dos = self._convert_dos(dos_energies, dos_values, unit_cell_volume = unit_cell_volume, n_atoms = n_atoms)
+        elif isinstance(convert_data, Callable):
+            energy, dos = convert_data(dos_energies, dos_values)
+        elif convert_data == None:
+            energy, dos = dos_energies, dos_values
+        else:
+            raise ValueError('Key-word argument ´convert_data´ must be either the string "Enc", a callable or None.')
         raw_energies, raw_dos = self._integrate_to_bins(energy, dos)
-        grid = Grid().create(grid_id = grid_id)
+        grid = Grid.create(grid_id = grid_id)
         self.grid_id = grid.get_grid_id()
         self.indices, self.bins = self._calculate_bytes(raw_energies, raw_dos, grid)
         return self
@@ -55,7 +63,7 @@ class DOSFingerprint():
         Performs stepwise numerical integration of ``ys`` over the range of ``xs``. The stepsize of the generated histogram is controlled by DOSFingerprint().stepsize.
         """
         if len(xs) < 2 or len(ys) < 2:
-            raise ValueError('Invalid input. Please provide arrays with len > 2.')
+            raise ValueError(f'Invalid input. Please provide arrays with len > 2. \n{xs}\n{ys}')
         xstart = round(int(xs[0] / (self.stepsize * 1.)) * self.stepsize, 8)  # define the limits that fit with the predefined stepsize
         xstop = round(int(xs[-1] / (self.stepsize * 1.)) * self.stepsize, 8)
         x_interp = np.arange(xstart, xstop + self.stepsize, self.stepsize)
@@ -82,7 +90,23 @@ class DOSFingerprint():
                 bin_dos += '0'
         return bin_dos
 
-    def _calculate_bytes(self, energy, dos, grid):
+    def _adapt_energy_bin_sizes(self, energy_bins: np.ndarray, states: np.ndarray, grid_array: list):
+        """
+        Adapt energy bin sizes to the specified values in the grid.
+        Args:
+            energy_bins: numpy.ndarray: locations of energy bins
+            states: numpy.ndarray: states in the bins declared in `energy_bins`
+            grid_array: list: discretisation of a Grid()-object
+        Returns:
+            None
+        """
+        # cut the energy and states to grid size
+        energy_bins, states = np.transpose([(e,d) for e,d in zip(energy_bins, states) if (e >= grid_array[0][0] and e <= grid_array[-1][0])])
+        # find grid start and end points
+        
+
+
+    def _calculate_bytes(self, energy, dos, grid, return_binned_dos = False):
         """
         Calculate the byte fingerprint.
         """
@@ -100,13 +124,19 @@ class DOSFingerprint():
                 break
         grid_start = grid_index
         fp_index = 0
+        binned_dos = []
+        grid_bins = []
         while grid_array[grid_index + 1][0] < energy[-1]:
             current_dos = 0
             while energy[fp_index] < grid_array[grid_index + 1][0]:
                 current_dos += dos[fp_index]
                 fp_index += 1
+            binned_dos.append(current_dos)
+            grid_bins.append(grid_array[grid_index][0])
             bin_fp += self._binary_bin(current_dos, grid_array[grid_index][1])
             grid_index += 1
         self.filling_factor = bin_fp.count('1') / len(bin_fp)
         byte_fp = bitarray(bin_fp).tobytes().hex()
+        if return_binned_dos:
+            return [grid_start, grid_index], byte_fp, [grid_bins, binned_dos]
         return [grid_start, grid_index], byte_fp
